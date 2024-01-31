42dcbdd8e27d5e56b40201d86b4152de9888d625
Degroshka <dmitr03@ngs.ru>
2024-01-31 14:45:03 +0700

    original

---
+++ Potato/postgres/01_init.sql
@@ -0,0 +1,3 @@
+create user wallet with login password 'wallet';
+
+create database wallet with owner = wallet;
---
+++ Potato/src/test/java/ru/cft/template/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package Potato.src.test.java.ru.cft.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
